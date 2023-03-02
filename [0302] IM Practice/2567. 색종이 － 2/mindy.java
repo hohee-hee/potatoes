8fe09639af39ed2d158f7aa2c8fb1fb62e3c8f15
김민정 <mmminjung0414@gmail.com>
2023-03-02 18:02:00 +0900

    i wanna go home

---
+++ [0302] IM Practice/2567. 색종이 － 2/mindy.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		int[][] arr = new int[100][100];
+		
+		int pap = sc.nextInt();
+		while(pap-- > 0) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			for(int i=0; i<10; i++) {
+				for(int j=0; j<10; j++) arr[y+i][x+j]++;
+			}
+		}
+		
+		int ans = 0;
+		for(int i=0; i<100; i++) {
+			for(int j=0; j<100; j++) {
+				if(arr[i][j] > 0) {
+					if(i-1 < 0) ans++;
+					if(i+1 >= 100) ans++;
+					if(j-1 < 0) ans++;
+					if(j+1 >= 100) ans++;
+					if(i-1 >= 0 && arr[i-1][j] == 0) ans++;
+					if(i+1 < 100 && arr[i+1][j] == 0) ans++;
+					if(j-1 >= 0 && arr[i][j-1] == 0) ans++;
+					if(j+1 < 100 && arr[i][j+1] == 0) ans++;
+				}
+			}
+		}
+		
+		System.out.println(ans);
+	}
+}
+
